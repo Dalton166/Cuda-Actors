c1ec199e4ac7d65aa5b937173cf047c344fe0021
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-06-11 14:52:58 -0600

    Updated mem_ref to be able to use caf::instrusive_ptr.

--- actor-framework/libcaf_cuda/caf/cuda/mem_ref.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/mem_ref.hpp
@@ -1,61 +1,59 @@
 #pragma once
 
-#include <stdexcept>
-
-
-
-
+#include <cuda.h>
+#include <caf/intrusive_ptr.hpp>
+#include <caf/ref_counted.hpp>
+#include "caf/cuda/global.hpp"
 
 namespace caf::cuda {
 
 template <class T>
-class mem_ref {
+class mem_ref : public caf::ref_counted {
 public:
   using value_type = T;
 
   mem_ref() = default;
 
-  /*
-  mem_ref(void*, void*, size_t) {
-    throw std::runtime_error("CUDA support disabled: mem_ref ctor");
-  }
-  */
-
-
-  //this is a default constructor that will likely be changed in the future
-  mem_ref(size_t num_elements, ,
-          CUdeviceptr memory, int access,
-          )
+  mem_ref(size_t num_elements, CUdeviceptr memory, int access)
     : num_elements_{num_elements},
-      access_{access},
-      memory_{memory} {
+      memory_{memory},
+      access_{access} {
     // nop
   }
 
-  ~mem_ref() { 
-	  reset();
+  ~mem_ref() {
+    reset();
   }
+
   mem_ref(mem_ref&&) noexcept = default;
   mem_ref& operator=(mem_ref&&) noexcept = default;
 
-  void* queue() const { return nullptr; }
-  void* mem() const { return nullptr; }
-  size_t size() const { return 0; }
+  // Disable copy
+  mem_ref(const mem_ref&) = delete;
+  mem_ref& operator=(const mem_ref&) = delete;
+
+  size_t size() const { return num_elements_; }
+  CUdeviceptr mem() const { return memory_; }
+  int access() const { return access_; }
 
   void reset() {
-  
-	  CuMemFree(memory_);
-	  num_elements = 0;
-	  memory_ = 0;
-	  access = NOT_IN_USE; 
+    if (memory_) {
+      cuMemFree(memory_);
+      memory_ = 0;
+    }
+    num_elements_ = 0;
+    access_ = -1;
   }
 
-
 private:
-  CUdeviceptr memory_;
-  int num_elements;
-  int access_;
-
+  size_t num_elements_ = 0;
+  CUdeviceptr memory_ = 0;
+  int access_ = -1;
 };
 
+// Alias for intrusive pointer to mem_ref<T>
+template <class T>
+using mem_ptr = caf::intrusive_ptr<mem_ref<T>>;
+
 } // namespace caf::cuda
+
