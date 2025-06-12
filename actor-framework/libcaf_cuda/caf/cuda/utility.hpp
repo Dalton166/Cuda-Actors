3d777c3e7206439f4451ba0734eac66d6bb835e7
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-06-12 12:43:10 -0600

    Added helper functions for requesting that a data set be turned into an arg.

--- actor-framework/libcaf_cuda/caf/cuda/utility.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/utility.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
 #include <cuda.h>
+#include "caf/cuda/types.hpp"
 #include "caf/cuda/manager.hpp"
 
+
+
+/*
+ * Most of these functions are just helper functions for 
+ * requesting a resource or operation from the manager
+ */
 namespace caf::cuda {
 
 // This helper calls manager singleton's get_context_by_id
@@ -10,6 +17,41 @@ inline CUcontext getContextById(int device_id, int context_id) {
   auto& mgr = manager::get();
   return mgr.get_context_by_id(device_id, context_id);
 }
+
+
+template< typename T>
+inline mem_ptr makeArg(int device_id,int context_id,in<T> arg) {
+
+	auto& mgr = manager::get();
+	device_ptr dev =  mgr.find_device(device_id);
+
+	return dev -> make_arg(arg);
+}	
+
+
+
+template< typename T>
+inline mem_ptr makeArg(int device_id,int context_id,in_out<T> arg) {
+
+	auto& mgr = manager::get();
+	device_ptr dev =  mgr.find_device(device_id);
+
+	return dev -> make_arg(arg);
+}	
+
+
+template< typename T>
+inline mem_ptr makeArg(int device_id,int context_id,out<T> arg) {
+
+	auto& mgr = manager::get();
+	device_ptr dev =  mgr.find_device(device_id);
+
+	return dev -> make_arg(arg);
+}	
+
+
+
+
 
 } // namespace caf::cuda
 
