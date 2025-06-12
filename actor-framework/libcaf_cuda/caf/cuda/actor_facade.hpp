537c7bd8867d388e9c4a5891b4cc524fbde972b4
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-06-12 13:07:43 -0600

    Implemented convert_data_to_args

--- actor-framework/libcaf_cuda/caf/cuda/actor_facade.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/actor_facade.hpp
@@ -21,26 +21,6 @@ namespace caf::cuda {
 template <bool PassConfig, class... Ts>
 class actor_facade : public caf::local_actor, public caf::resumable {
 public:
-  //static caf::actor create(...) {
-    //throw std::runtime_error("CUDA support disabled: actor_facade::create()");
-  //}
-
-  /*
-  static caf::actor create(
-    caf::actor_system& sys,
-    caf::actor_config&& actor_conf,
-    Ts&&... xs
-  ) {
-    std::cout << "Actor has successfully spawned and was created\n";
-    return make_actor<actor_facade, actor>(
-      sys.next_actor_id(),
-      sys.node(),
-      &sys,
-      std::move(actor_conf),
-      std::forward<Ts>(xs)...);
-  }
-
-  */
   static caf::actor create(
     caf::actor_system& sys,
     caf::actor_config&& actor_conf,
@@ -74,25 +54,10 @@ public:
   }
 
 
-
-
-
-
   actor_facade(caf::actor_config& cfg) : caf::local_actor(cfg), caf::resumable() {
     throw std::runtime_error("CUDA support disabled: actor_facade ctor");
   }
 
-
-  /*
-  actor_facade(caf::actor_config&& cfg, Ts&&... xs)
-    : local_actor(cfg),
-      resumable(),
-      config_(std::move(cfg)) {
-    std::cout << "Actor has successfully spawned and was created\n";
-    print_args(std::forward<Ts>(xs)...);
-  }
-
-  */
 
   actor_facade(caf::actor_config&& cfg,program_ptr prog,Ts&&... xs)
     : local_actor(cfg),
@@ -104,9 +69,30 @@ public:
   }
 
 
+ template <typename... Args>
+std::tuple<mem_ptr...> convert_data_to_args(Args&&... args) {
+  int dev_id = program_->get_device_id();
+  int ctx_id = program_->get_context_id();
+  return std::make_tuple(makeArg(dev_id, ctx_id, std::forward<Args>(args))...);
+}
 
 
+private:
+  caf::actor_config config_;
+  std::queue<mailbox_element_ptr> mailbox_; // Mailbox for messages
+  program_ptr program_;
 
+  void print_args() {
+    std::cout << "(no args)\n";
+  }
+
+  template <typename T, typename... Rest>
+  void print_args(T&& first, Rest&&... rest) {
+    std::cout << first << "\n";
+    print_args(std::forward<Rest>(rest)...);
+  }
+
+//-------------------------Basic implementation, will need to implement fully later
   // Implement caf::resumable interface
   subtype_t subtype() const noexcept override {
     return subtype_t(0); // Placeholder: minimal type identifier
@@ -151,20 +137,8 @@ public:
     }
   }
 
-private:
-  caf::actor_config config_;
-  std::queue<mailbox_element_ptr> mailbox_; // Mailbox for messages
-  program_ptr program_;
 
-  void print_args() {
-    std::cout << "(no args)\n";
-  }
 
-  template <typename T, typename... Rest>
-  void print_args(T&& first, Rest&&... rest) {
-    std::cout << first << "\n";
-    print_args(std::forward<Rest>(rest)...);
-  }
 };
 
 } // namespace caf::cuda
