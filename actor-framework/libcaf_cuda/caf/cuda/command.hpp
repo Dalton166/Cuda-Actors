84cbe29842d72b720fe72e49799cda7f54b0ce52
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-06-18 13:59:23 -0600

    Added new helper functions to convert arguments instead of using the ones in utility.hpp, this change was made since we can;t use manager here.

--- actor-framework/libcaf_cuda/caf/cuda/command.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/command.hpp
@@ -1,26 +1,10 @@
-/******************************************************************************
- *                       ____    _    _____                                   *
- *                      / ___|  / \  |  ___|    C++                           *
- *                     | |     / _ \ | |_       Actor                         *
- *                     | |___ / ___ \|  _|      Framework                     *
- *                      \____/_/   \_|_|                                      *
- *                                                                            *
- * Copyright (C) 2011 - 2016                                                  *
- *                                                                            *
- * Distributed under the terms and conditions of the BSD 3-Clause License or  *
- * (at yourਸ  your option) under the terms and conditions of the Boost Software      *
- * License 1.0. See accompanying files LICENSE and LICENSE_ALTERNATIVE.       *
- *                                                                            *
- * If you did not receive a copy of the license files, see                    *
- * http://opensource.org/licenses/BSD-3-Clause and                            *
- * http://www.boost.org/LICENSE_1_0.txt.                                      *
- ******************************************************************************/
-
 #pragma once
 
 #include <tuple>
 #include <vector>
 #include <functional>
+#include <atomic>
+#include <iostream>
 
 #include <caf/abstract_actor.hpp>
 #include <caf/actor_cast.hpp>
@@ -34,49 +18,105 @@
 #include "caf/cuda/nd_range.hpp"
 #include "caf/cuda/arguments.hpp"
 #include "caf/cuda/opencl_err.hpp"
-//#include "caf/cuda/manager.hpp"
 #include "caf/cuda/device.hpp"
 //#include "caf/cuda/utility.hpp"
-
 
 namespace caf::cuda {
 
 template <class Actor, class... Ts>
 class command : public ref_counted {
 public:
-//  using result_types = caf::detail::type_list<Ts...>;
-
- command(caf::response_promise promise,
-          program_ptr program, nd_range dims,
-          Ts&&... xs)
+  // Constructor template to perfectly forward argument wrappers (in/out/in_out)
+  template <typename... Us>
+  command(caf::response_promise promise,
+          program_ptr program,
+          nd_range dims,
+          Us&&... xs)
     : rp(std::move(promise)),
-      program_(program),
+      program_(std::move(program)),
       dims_(dims),
-      mem_refs(convert_data_to_args(std::forward<Ts>(xs)...)) {
+      mem_refs(convert_data_to_args(std::forward<Us>(xs)...)) {
+    static_assert(sizeof...(Us) == sizeof...(Ts), "Argument count mismatch");
   }
-
 
   void enqueue() {
-  
-	  launch_kernel(program_,dims_,mem_refs,program_ -> get_stream_id());
+    launch_kernel(program_, dims_, mem_refs, program_->get_stream_id());
 
-	  //at this point for now the kernel should be done running and we should have our data in mem_ref ready to be transfer back to the device as well as cleanup 
-	 print_and_cleanup_outputs(mem_refs);
-  
+    // At this point kernel should be done, data in mem_refs ready to copy back and cleanup
+    print_and_cleanup_outputs(mem_refs);
   }
 
-  //TODO if command is ever deconstructed they should free all the mem_refs they have on them 
   ~command() override = default;
 
+private:
+  // Convert argument wrappers to mem_ptr of raw types
+  template <typename... Args>
+  std::tuple<mem_ptr<raw_t<std::decay_t<Args>>>...>
+  convert_data_to_args(Args&&... args) {
+    int dev_id = program_->get_device_id();
+    int ctx_id = program_->get_context_id();
+    return std::make_tuple(makeArg(dev_id, ctx_id, std::forward<Args>(args))...);
+  }
 
-  // Implementation of intrusive_ptr_add_ref increments ref count
-void intrusive_ptr_add_ref(command* ptr) {
+  // Helper to iterate over tuple elements
+  template <typename Tuple, typename Func, size_t... Is>
+  void for_each_tuple_impl(Tuple& t, Func&& f, std::index_sequence<Is...>) {
+    (f(std::get<Is>(t)), ...);
+  }
+
+  template <typename... Is, typename Func>
+  void for_each_tuple(std::tuple<Is...>& t, Func&& f) {
+    for_each_tuple_impl(t, std::forward<Func>(f), std::index_sequence_for<Is...>{});
+  }
+
+  // Print output buffers and cleanup mem_refs
+  void print_and_cleanup_outputs(std::tuple<mem_ptr<raw_t<Ts>>...>& mem_refs) {
+    for_each_tuple(mem_refs, [](auto& mem) {
+      if (!mem)
+        return;
+
+      const int access = mem->access();
+      if (access == OUT || access == IN_OUT) {
+        auto host_data = mem->copy_to_host();
+        std::cout << "Output buffer (" << host_data.size() << "): ";
+        for (const auto& val : host_data) {
+          std::cout << val << " ";
+        }
+        std::cout << '\n';
+      }
+
+      mem->reset(); // Always clean up
+    });
+  }
+
+  void launch_kernel(program_ptr program,
+                     const caf::cuda::nd_range& range,
+                     std::tuple<mem_ptr<raw_t<Ts>> ...> args,
+                     int stream_id) {
+
+    int device_id = program->get_device_id();
+    int context_id = program->get_context_id();
+    CUfunction kernel = program->get_kernel();
+    device_ptr dev = program->get_device();
+    dev->launch_kernel(kernel, range, args, stream_id, context_id);
+  }
+
+  program_ptr program_;
+  caf::response_promise rp;
+  nd_range dims_;
+  std::tuple<mem_ptr<raw_t<Ts>>...> mem_refs;
+  std::atomic<int> ref_count{0};
+};
+
+// Free functions for intrusive_ptr ref counting to enable ADL
+template <class Actor, class... Ts>
+inline void intrusive_ptr_add_ref(command<Actor, Ts...>* ptr) {
   ++(ptr->ref_count);
   std::cout << "intrusive_ptr_add_ref: ref_count=" << ptr->ref_count.load() << "\n";
 }
 
-// Implementation of intrusive_ptr_release decrements ref count and deletes if zero
-void intrusive_ptr_release(command* ptr) {
+template <class Actor, class... Ts>
+inline void intrusive_ptr_release(command<Actor, Ts...>* ptr) {
   if (--(ptr->ref_count) == 0) {
     std::cout << "intrusive_ptr_release: deleting command\n";
     delete ptr;
@@ -85,64 +125,39 @@ void intrusive_ptr_release(command* ptr) {
   }
 }
 
-
-private:
-  template <typename... Args>
-  std::tuple<mem_ptr<std::decay_t<Args>>...>
-  convert_data_to_args(Args&&... args) {
-    int dev_id = program_->get_device_id();
-    int ctx_id = program_->get_context_id();
-    return std::make_tuple(makeArg(dev_id, ctx_id, std::forward<Args>(args))...);
+// Helper makeArg overloads using program_->get_device()
+  template <typename T>
+  mem_ptr<T> makeArg(in<T> arg) {
+    device_ptr dev = program_->get_device();
+    return dev->make_arg(arg);
   }
 
-  template <typename Tuple, typename Func, size_t... Is>
-void for_each_tuple_impl(Tuple& t, Func&& f, std::index_sequence<Is...>) {
-  (f(std::get<Is>(t)), ...);
-}
+  template <typename T>
+  mem_ptr<T> makeArg(in_out<T> arg) {
+    device_ptr dev = program_->get_device();
+    return dev->make_arg(arg);
+  }
 
-template <typename... Is, typename Func>
-void for_each_tuple(std::tuple<Is...>& t, Func&& f) {
-  for_each_tuple_impl(t, std::forward<Func>(f), std::index_sequence_for<Is...>{});
-}
+  template <typename T>
+  mem_ptr<T> makeArg(out<T> arg) {
+    device_ptr dev = program_->get_device();
+    return dev->make_arg(arg);
+  }
 
-// Iterate, print host data if access is OUT or IN_OUT, and cleanup
-void print_and_cleanup_outputs(std::tuple<mem_ptr<Ts>...>& mem_refs) {
-  for_each_tuple(mem_refs, [](auto& mem) {
-    if (!mem)
-      return;
+  // fallback template for other types (if any)
+  template <typename T>
+  mem_ptr<T> makeArg(T&& arg) {
+    device_ptr dev = program_->get_device();
+    return dev->make_arg(std::forward<T>(arg));
+  }
 
-    const int access = mem->access();
-    if (access == OUT || access == IN_OUT) {
-      auto host_data = mem->copy_to_host();
-      std::cout << "Output buffer (" << host_data.size() << "): ";
-      for (const auto& val : host_data) {
-        std::cout << val << " ";
-      }
-      std::cout << '\n';
-    }
+  // Now your convert_data_to_args becomes:
+  template <typename... Args>
+  auto convert_data_to_args(Args&&... args) {
+    // Call the member makeArg overloads (no need for device_id/context_id params)
+    return std::make_tuple(makeArg(std::forward<Args>(args))...);
+  }
 
-    mem->reset(); // Always clean up
-  });
-}
-void launch_kernel(program_ptr program,
-                   const caf::cuda::nd_range& range,
-                   std::tuple<mem_ptr<raw_t<Ts>> ...> args,
-		   int stream_id) {
-
-	int device_id = program -> get_device_id();
-	int context_id = program -> get_context_id();
-	CUfunction kernel = program -> get_kernel();
-	//auto& mgr = manager::get();
-	device_ptr dev =  program -> get_device();
-	dev -> launch_kernel(kernel,range,args,stream_id,context_id);
-}
-
-  program_ptr program_;
-  caf::response_promise rp;
-  int flags = 0;
-  std::tuple<mem_ptr<std::decay_t<Ts>>...> mem_refs;
-  nd_range dims_;
- std::atomic<int> ref_count{0};
-};
 
 } // namespace caf::cuda
+
