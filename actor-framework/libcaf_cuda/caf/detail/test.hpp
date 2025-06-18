da2b48c21d7e14e1df2f245ad0492fff61d3538e
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-06-18 10:24:15 -0600

    Added more stubs

--- actor-framework/libcaf_cuda/caf/detail/test.hpp
+++ actor-framework/libcaf_cuda/caf/detail/test.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <caf/all.hpp>
-#include "caf/cuda/manager.hpp"
-#include "caf/cuda/program.hpp"
-#include "caf/cuda/device.hpp"
-#include "caf/cuda/command.hpp"
-#include "caf/cuda/actor_facade.hpp"
-#include "caf/cuda/mem_ref.hpp"
-#include "caf/cuda/platform.hpp"
+#include <caf/cuda/manager.hpp>
+#include <caf/cuda/program.hpp>
+#include <caf/cuda/device.hpp>
+#include <caf/cuda/command.hpp>
+#include <caf/cuda/actor_facade.hpp>
+#include <caf/cuda/mem_ref.hpp>
+#include <caf/cuda/platform.hpp>
+#include <caf/cuda/global.hpp>
 
 // Test function declarations
 void test_platform(caf::actor_system& sys);
@@ -17,3 +18,4 @@ void test_program(caf::actor_system& sys);
 void test_mem_ref(caf::actor_system& sys);
 void test_command(caf::actor_system& sys);
 void test_actor_facade(caf::actor_system& sys);
+void test_main(caf::actor_system& sys);
