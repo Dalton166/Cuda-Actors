61205e5ec555c9adbb67182fe2d3fdd4c55468b6
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-06-18 09:52:59 -0600

    Adding a test file to this library.

---
+++ actor-framework/libcaf_cuda/caf/detail/test.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <caf/all.hpp>
+#include "caf/cuda/manager.hpp"
+#include "caf/cuda/program.hpp"
+#include "caf/cuda/device.hpp"
+#include "caf/cuda/command.hpp"
+#include "caf/cuda/actor_facade.hpp"
+#include "caf/cuda/mem_ref.hpp"
+#include "caf/cuda/platform.hpp"
+
+// Test function declarations
+void test_platform(caf::actor_system& sys);
+void test_device(caf::actor_system& sys);
+void test_manager(caf::actor_system& sys);
+void test_program(caf::actor_system& sys);
+void test_mem_ref(caf::actor_system& sys);
+void test_command(caf::actor_system& sys);
+void test_actor_facade(caf::actor_system& sys);
