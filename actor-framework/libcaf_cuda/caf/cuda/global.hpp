ff3e9380cd8eaa0a3e320cfd7c8dc9833378b8bd
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-07-07 12:44:42 -0600

    Updated the wrapper types and their construction to turn it into a shared pointer to hopefully avoid caf serialization overhead.

--- actor-framework/libcaf_cuda/caf/cuda/global.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/global.hpp
@@ -3,84 +3,91 @@
 #include <string>
 #include <iostream>
 #include <stdexcept>
+#include <memory>
 
 #include <caf/logger.hpp>
 #include <cuda.h>
-#include "caf/cuda/types.hpp" //be sure to include any caf-cuda headers after this one
+#include "caf/cuda/types.hpp" // be sure to include any caf-cuda headers after this one
 #include "caf/cuda/nd_range.hpp"
 #include "caf/cuda/cuda-actors.hpp"
 #include <nvrtc.h>
 // CAF type ID registration
 #include <caf/type_id.hpp>
 
-
-
-
-
-
-//a strange fix required in order to get the .so files to become viewable for binaries
-//linking against them, if this is not defined with classes you want viewable then
-//the linker will complain
+// a strange fix required in order to get the .so files to become viewable for binaries
+// linking against them, if this is not defined with classes you want viewable then
+// the linker will complain
 #if defined(_MSC_VER)
   #define CAF_CUDA_EXPORT __declspec(dllexport)
 #else
   #define CAF_CUDA_EXPORT __attribute__((visibility("default")))
 #endif
 
-
-
-//memory access flags, required for identifying which
-//gpu buffers are input and output buffers
+// memory access flags, required for identifying which
+// gpu buffers are input and output buffers
 #define IN 0 
 #define IN_OUT 1
 #define OUT 2
 #define NOT_IN_USE -1
 
-
-//argument tags
+// argument tags
 struct input_tag {};
 struct output_tag {};
 struct input_output_tag {};
 
-
-//struct wrappers to hold store buffers to declare them as in or out 
+// struct wrappers to hold store buffers to declare them as in or out 
 template <typename T>
 struct in {
-  std::vector<T> buffer;
+  std::shared_ptr<std::vector<T>> buffer_ptr;
 
   in() = default;
 
-  // Construct from a single raw value by pushing it into buffer
-  in(T val) {
-    buffer.push_back(val);
-  }
+  explicit in(std::shared_ptr<std::vector<T>> ptr) : buffer_ptr(std::move(ptr)) {}
+
+  explicit in(const std::vector<T>& vec)
+    : buffer_ptr(std::make_shared<std::vector<T>>(vec)) {}
+
+  explicit in(std::vector<T>&& vec)
+    : buffer_ptr(std::make_shared<std::vector<T>>(std::move(vec))) {}
+
+  // Construct from a single raw value by pushing it into a vector
+  in(T val) : buffer_ptr(std::make_shared<std::vector<T>>(1, val)) {}
 };
 
 template <typename T>
 struct out {
-  std::vector<T> buffer;
+  std::shared_ptr<std::vector<T>> buffer_ptr;
 
   out() = default;
 
-  out(T val) {
-    buffer.push_back(val);
-  }
+  explicit out(std::shared_ptr<std::vector<T>> ptr) : buffer_ptr(std::move(ptr)) {}
+
+  explicit out(const std::vector<T>& vec)
+    : buffer_ptr(std::make_shared<std::vector<T>>(vec)) {}
+
+  explicit out(std::vector<T>&& vec)
+    : buffer_ptr(std::make_shared<std::vector<T>>(std::move(vec))) {}
+
+  out(T val) : buffer_ptr(std::make_shared<std::vector<T>>(1, val)) {}
 };
-
-
 
 template <typename T>
 struct in_out {
-    using value_type = T;
-    std::vector<T> buffer;
+  using value_type = T;
+  std::shared_ptr<std::vector<T>> buffer_ptr;
 
-    in_out() = default;
+  in_out() = default;
 
-    in_out(T val) {
-      buffer.push_back(val);
-    }
+  explicit in_out(std::shared_ptr<std::vector<T>> ptr) : buffer_ptr(std::move(ptr)) {}
+
+  explicit in_out(const std::vector<T>& vec)
+    : buffer_ptr(std::make_shared<std::vector<T>>(vec)) {}
+
+  explicit in_out(std::vector<T>&& vec)
+    : buffer_ptr(std::make_shared<std::vector<T>>(std::move(vec))) {}
+
+  in_out(T val) : buffer_ptr(std::make_shared<std::vector<T>>(1, val)) {}
 };
-
 
 // Helper to get raw type inside wrapper
 template <typename T>
@@ -107,9 +114,7 @@ struct raw_type<in_out<T>> {
 template <typename T>
 using raw_t = typename raw_type<T>::type;
 
-
-
-//helper function to check errors
+// helper function to check errors
 void inline check(CUresult result, const char* msg) {
     if (result != CUDA_SUCCESS) {
         const char* err_str;
@@ -119,36 +124,32 @@ void inline check(CUresult result, const char* msg) {
     }
 }
 
-
-
+// Updated CAF inspect functions to serialize shared_ptr only
 template <class Inspector, typename T>
 bool inspect(Inspector& f, in<T>& x) {
-  return f.object(x).fields(f.field("buffer", x.buffer));
+  return f.object(x).fields(f.field("buffer_ptr", x.buffer_ptr));
 }
 
 template <class Inspector, typename T>
 bool inspect(Inspector& f, out<T>& x) {
-  return f.object(x).fields(f.field("buffer", x.buffer));
+  return f.object(x).fields(f.field("buffer_ptr", x.buffer_ptr));
 }
 
 template <class Inspector, typename T>
 bool inspect(Inspector& f, in_out<T>& x) {
-  return f.object(x).fields(f.field("buffer", x.buffer));
+  return f.object(x).fields(f.field("buffer_ptr", x.buffer_ptr));
 }
 
 using buffer_variant = std::variant<std::vector<char>, std::vector<int>, std::vector<float>, std::vector<double>>;
 
 struct output_buffer {
   buffer_variant data;
-
 };
-
 
 template <class Inspector>
 bool inspect(Inspector& f, output_buffer& x) {
   return f.object(x).fields(f.field("data", x.data));
 }
-
 
 // Check CUDA errors macro
 #define CHECK_CUDA(call) \
@@ -160,7 +161,6 @@ bool inspect(Inspector& f, output_buffer& x) {
 #define CHECK_NVRTC(call) \
     do { nvrtcResult res = call; if (res != NVRTC_SUCCESS) { \
         std::cerr << "NVRTC Error: " << nvrtcGetErrorString(res) << std::endl; exit(1); }} while(0)
-
 
 // CAF type ID registration
 #include <caf/type_id.hpp>
@@ -178,6 +178,11 @@ CAF_BEGIN_TYPE_ID_BLOCK(cuda, caf::first_custom_type_id)
   CAF_ADD_TYPE_ID(cuda, (std::vector<float>))
   CAF_ADD_TYPE_ID(cuda, (std::vector<double>))
 
+  // Add registrations for shared_ptr versions
+  CAF_ADD_TYPE_ID(cuda, (std::shared_ptr<std::vector<int>>))
+  CAF_ADD_TYPE_ID(cuda, (std::shared_ptr<std::vector<float>>))
+  CAF_ADD_TYPE_ID(cuda, (std::shared_ptr<std::vector<double>>))
+
   CAF_ADD_TYPE_ID(cuda, (buffer_variant))
 
   CAF_ADD_TYPE_ID(cuda, (output_buffer))
@@ -187,7 +192,6 @@ CAF_BEGIN_TYPE_ID_BLOCK(cuda, caf::first_custom_type_id)
   CAF_ADD_ATOM(cuda, kernel_done_atom)
 
 CAF_END_TYPE_ID_BLOCK(cuda)
-
 
 namespace caf::cuda {
 
@@ -207,3 +211,4 @@ inline std::string event_status(void* /*event*/) {
 //}
 
 } // namespace caf::cuda
+
--- actor-framework/libcaf_cuda/caf/cuda/utility.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/utility.hpp
@@ -4,8 +4,6 @@
 #include "caf/cuda/types.hpp"
 #include "caf/cuda/manager.hpp"
 
-
-
 /*
  * Most of these functions are just helper functions for 
  * requesting a resource or operation from the manager
@@ -18,83 +16,39 @@ inline CUcontext getContextById(int device_id, int context_id) {
   return mgr.get_context_by_id(device_id, context_id);
 }
 
-
-
-/*
-//helper function to launch a kernel for the command class 
-template <typename ... Ts>
-inline void launch_kernel(program_ptr program,
-                   const caf::cuda::nd_range& range,
-                   std::tuple<mem_ptr<Ts> ...> args,
-		   int stream_id) {
-
-	
-	int device_id = program -> get_device_id();
-	int context_id = program -> get_context_id();
-	CUfunction kernel = program -> get_kernel();
-	auto& mgr = manager::get();
-	device_ptr dev =  mgr.find_device(device_id);
-	dev -> launch_kernel(kernel,range,args,stream_id,context_id);
-
-}
-*/
-
-
 template< typename T>
 inline mem_ptr<T> makeArg(int device_id,int context_id,in<T> arg) {
-
-	auto& mgr = manager::get();
-	device_ptr dev =  mgr.find_device(device_id);
-
-	return dev -> make_arg(arg);
-}	
-
-
+  auto& mgr = manager::get();
+  device_ptr dev =  mgr.find_device(device_id);
+  return dev->make_arg(arg);
+}
 
 template< typename T>
 inline mem_ptr<T> makeArg(int device_id,int context_id,in_out<T> arg) {
-
-	auto& mgr = manager::get();
-	device_ptr dev =  mgr.find_device(device_id);
-
-	return dev -> make_arg(arg);
-}	
-
-
-template <typename T>
-inline mem_ptr <T> makeArg(int device_id,int context_id,out<T> arg) {
-
-	auto& mgr = manager::get();
-	device_ptr dev =  mgr.find_device(device_id);
-
-	return dev -> make_arg(arg);
-}	
-
-template <typename T>
-inline in<T> create_in_arg(std::vector<T> buffer) {
-
-	in<T> arg;
-	arg.buffer = buffer;
-	return arg;
+  auto& mgr = manager::get();
+  device_ptr dev =  mgr.find_device(device_id);
+  return dev->make_arg(arg);
 }
 
 template <typename T>
-inline in_out<T> create_in_out_arg(std::vector<T> buffer) {
-
-	in_out<T> arg;
-	arg.buffer = buffer;
-	return arg;
+inline mem_ptr<T> makeArg(int device_id,int context_id,out<T> arg) {
+  auto& mgr = manager::get();
+  device_ptr dev =  mgr.find_device(device_id);
+  return dev->make_arg(arg);
+}
+template <typename T>
+inline in<T> create_in_arg(const std::vector<T>& buffer) {
+  return in<T>(std::make_shared<std::vector<T>>(buffer)); // copy constructor
 }
 
 template <typename T>
-inline out<T> create_out_arg(std::vector<T> buffer) {
-
-	out<T> arg;
-	arg.buffer = buffer;
-	return arg;
+inline in_out<T> create_in_out_arg(const std::vector<T>& buffer) {
+  return in_out<T>(std::make_shared<std::vector<T>>(buffer)); // copy constructor
 }
 
-
+template <typename T>
+inline out<T> create_out_arg(const std::vector<T>& buffer) {
+  return out<T>(std::make_shared<std::vector<T>>(buffer)); // copy constructor
+}
 
 } // namespace caf::cuda
-
