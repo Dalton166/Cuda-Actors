bff132d95f4e1593b2051bc6c692b924ebf9a106
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-07-24 12:48:07 -0600

    Moved implementations in platform.hpp to platform.cpp

--- actor-framework/libcaf_cuda/src/platform.cpp
+++ actor-framework/libcaf_cuda/src/platform.cpp
@@ -1,58 +1,88 @@
-/******************************************************************************
- *                       ____    _    _____                                   *
- *                      / ___|  / \  |  ___|    C++                           *
- *                     | |     / _ \ | |_       Actor                         *
- *                     | |___ / ___ \|  _|      Framework                     *
- *                      \____/_/   \_|_|                                      *
- *                                                                            *
- * Copyright (C) 2011 - 2016                                                  *
- *                                                                            *
- * Distributed under the terms and conditions of the BSD 3-Clause License or  *
- * (at your option) under the Boost Software      *
- * License 1.0. See accompanying files LICENSE and LICENSE_ALTERNATIVE.       *
- *                                                                            *
- * If you did not receive a copy of the license files, see                    *
- * http://opensource.org/licenses/BSD-3-Clause and                            *
- * http://www.boost.org/LICENSE_1_0.txt.                                      *
- ******************************************************************************/
-
 #include "caf/cuda/platform.hpp"
-
 #include <iostream>
-/*
-namespace caf {
-namespace cuda {
 
-platform::platform(cl_platform_id id, unsigned& current_device_id)
-    : id_(id) {
-  cl_uint num_devices = 0;
-  clGetDeviceIDs(id, CL_DEVICE_TYPE_ALL, 0, nullptr, &num_devices);
-  if (num_devices == 0)
-    return;
-  std::vector<cl_device_id> device_ids(num_devices);
-  clGetDeviceIDs(id, CL_DEVICE_TYPE_ALL, num_devices, device_ids.data(),
-                 nullptr);
-  devices_.reserve(num_devices);
-  for (auto dev_id : device_ids) {
-    devices_.emplace_back(std::make_shared<device>(dev_id, context_, queue_));
-    ++current_device_id;
+namespace caf::cuda {
+
+platform_ptr platform::create() {
+  static platform_ptr instance;
+  if (!instance) {
+    std::cout << "[platform] constructor called\n";
+    instance = make_counted<platform>();
+  }
+  return instance;
+}
+
+platform::platform() {
+  int device_count = 0;
+  check(cuDeviceGetCount(&device_count), "cuDeviceGetCount");
+  devices_.resize(device_count);
+  contexts_.resize(device_count);
+
+  std::cout << "Device count is " << device_count << " \n";
+
+  for (int i = 0; i < device_count; ++i) {
+    CUdevice cuda_device;
+    check(cuDeviceGet(&cuda_device, i), "cuDeviceGet");
+
+    char name[256];
+    cuDeviceGetName(name, 256, cuda_device);
+
+    check(cuCtxCreate(&contexts_[i], CU_CTX_SCHED_AUTO | CU_CTX_MAP_HOST, cuda_device), "cuCtxCreate");
+    devices_[i] = make_counted<device>(cuda_device, contexts_[i], name, i);
+  }
+
+  scheduler_ = (device_count <= 1)
+      ? std::make_unique<single_device_scheduler>()
+      : std::make_unique<multi_device_scheduler>();
+
+  scheduler_->set_devices(devices_);
+
+  if (device_count > 0) {
+    check(cuCtxSetCurrent(contexts_[0]), "cuCtxSetCurrent");
   }
 }
 
 platform::~platform() {
-  // no explicit cleanup
+  std::cout << "Destroying platform\n";
+  // Context cleanup is handled in device destructors
 }
 
-std::string platform::name() const {
-  size_t len = 0;
-  clGetPlatformInfo(id_, CL_PLATFORM_NAME, 0, nullptr, &len);
-  std::string result(len, '\0');
-  clGetPlatformInfo(id_, CL_PLATFORM_NAME, len, result.data(), nullptr);
-  if (!result.empty() && result.back() == '\0')
-    result.pop_back();
-  return result;
+const std::string& platform::name() const {
+  return name_;
 }
 
-} // namespace opencl
-} // namespace caf
-*/
+const std::string& platform::vendor() const {
+  return vendor_;
+}
+
+const std::string& platform::version() const {
+  return version_;
+}
+
+const std::vector<device_ptr>& platform::devices() const {
+  return devices_;
+}
+
+device_ptr platform::getDevice(int id) {
+  if (id < 0 || static_cast<size_t>(id) >= devices_.size()) {
+    throw std::out_of_range("Invalid device ID");
+  }
+  return devices_[id];
+}
+
+scheduler* platform::get_scheduler() {
+  return scheduler_.get();
+}
+
+device_ptr platform::schedule(int actor_id) {
+  return scheduler_->schedule(actor_id);
+}
+
+void platform::release_streams_for_actor(int actor_id) {
+  for (auto& dev : devices_) {
+    dev->release_stream_for_actor(actor_id);
+  }
+}
+
+} // namespace caf::cuda
+
