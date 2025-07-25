13bf0c26f61332ca8727a82ff38792a0579d51f2
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-07-25 10:24:47 -0600

    Fixed linker visibility issue with random number as well as put the function in the correct namespace.

--- actor-framework/libcaf_cuda/src/helpers.cpp
+++ actor-framework/libcaf_cuda/src/helpers.cpp
@@ -1,12 +1,12 @@
 #include "caf/cuda/helpers.hpp"
 
-
+namespace caf::cuda {
 //gets a random number
-int random_number() {
+CAF_CUDA_EXPORT int random_number() {
     static std::random_device rd;
     static std::mt19937 gen(rd());
     static std::uniform_int_distribution<int> distrib(INT_MIN, INT_MAX);
     return distrib(gen);  
 }
 
-
+}
