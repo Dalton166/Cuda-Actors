0cf23b738137f3561dbd580ffca87566ba760c54
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-07-25 10:06:58 -0600

    Initial commit.

---
+++ actor-framework/libcaf_cuda/caf/cuda/helpers.hpp
@@ -0,0 +1,13 @@
+/*
+ * A file full of helper functions that can be called by any class at anytime 
+ * if desired 
+ */
+
+#include "caf/cuda/global.hpp"
+#include <random>
+
+namespace caf::cuda {
+
+
+
+}
