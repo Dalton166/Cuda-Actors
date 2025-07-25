e00dcd0533ac8bc018334479f968e434c354ff57
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-07-25 12:14:21 -0600

    Added compile_nvrtc program stub.

--- actor-framework/libcaf_cuda/caf/cuda/helpers.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/helpers.hpp
@@ -11,4 +11,6 @@ namespace caf::cuda {
 
 	CAF_CUDA_EXPORT int random_number();
 	CAF_CUDA_EXPORT std::vector<char> load_cubin(const std::string&);
+	CAF_CUDA_EXPORT bool compile_nvrtc_program(const char* source, CUdevice device, std::vector<char>& ptx_out);
+
 }
