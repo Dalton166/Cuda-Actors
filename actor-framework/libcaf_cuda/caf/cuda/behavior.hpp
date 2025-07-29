dcb8bd8ddd6d2d1aac76682455a68840fe0fc637
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-07-29 13:34:46 -0600

    Updated behavior to return an std::tuple of its wrapper types.

--- actor-framework/libcaf_cuda/caf/cuda/behavior.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/behavior.hpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <tuple>
 #include <type_traits>
+#include <utility>
 
 namespace caf::cuda {
 
@@ -15,52 +16,49 @@ public:
   virtual ~behavior_base() = default;
 };
 
+// Main template
 template <typename... Args>
 class behavior : public behavior_base {
 public:
-  using args_tuple = std::tuple<Args...>;
+  using tag_types = std::tuple<Args...>;
+  using raw_types = std::tuple<raw_t<Args>...>;
   using preprocess_fn = std::function<void(caf::message&)>;
   using postprocess_fn = std::function<void(output_buffer&)>;
 
   behavior(program_ptr prog,
            nd_range range,
            std::string name,
-           std::optional<preprocess_fn> pre = std::nullopt,
-           std::optional<postprocess_fn> post = std::nullopt)
+           std::optional<preprocess_fn> pre,
+           std::optional<postprocess_fn> post,
+           Args&&... args)
     : prog_(std::move(prog)),
       range_(std::move(range)),
       name_(std::move(name)),
       pre_(std::move(pre)),
-      post_(std::move(post)) {}
+      post_(std::move(post)),
+      args_tuple_(std::forward<Args>(args)...) {}
 
-  // Getters
-  const program_ptr& program() const noexcept {
-    return prog_;
-  }
+  // Overload for no pre/post lambdas (optional)
+  behavior(program_ptr prog,
+           nd_range range,
+           std::string name,
+           Args&&... args)
+    : prog_(std::move(prog)),
+      range_(std::move(range)),
+      name_(std::move(name)),
+      pre_(std::nullopt),
+      post_(std::nullopt),
+      args_tuple_(std::forward<Args>(args)...) {}
 
-  const nd_range& range() const noexcept {
-    return range_;
-  }
+  // === Getters ===
+  const program_ptr& program() const noexcept { return prog_; }
+  const nd_range& range() const noexcept { return range_; }
+  const std::string& name() const noexcept { return name_; }
+  const std::optional<preprocess_fn>& preprocessor() const noexcept { return pre_; }
+  const std::optional<postprocess_fn>& postprocessor() const noexcept { return post_; }
+  const tag_types& arg_tags() const noexcept { return args_tuple_; }
 
-  const std::string& name() const noexcept {
-    return name_;
-  }
-
-  const std::optional<preprocess_fn>& preprocessor() const noexcept {
-    return pre_;
-  }
-
-  const std::optional<postprocess_fn>& postprocessor() const noexcept {
-    return post_;
-  }
-
-  std::tuple<Args...> args() const {
-    return {};
-  }
-
-  static constexpr std::size_t num_args() noexcept {
-    return sizeof...(Args);
-  }
+  static constexpr std::size_t num_args() noexcept { return sizeof...(Args); }
 
 private:
   program_ptr prog_;
@@ -68,6 +66,7 @@ private:
   std::string name_;
   std::optional<preprocess_fn> pre_;
   std::optional<postprocess_fn> post_;
+  tag_types args_tuple_;
 };
 
 } // namespace caf::cuda
