d5ff274be908582d7dc82d164e5b2bb4c1445dae
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-07-31 13:22:40 -0600

    Changed behavior_base_ptr to behavior_ptr.

--- actor-framework/libcaf_cuda/caf/cuda/behavior.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/behavior.hpp
@@ -87,7 +87,7 @@ public:
 };
 
 
-using behavior_base_ptr = caf::intrusive_ptr<behavior_base>;
+using behavior_ptr = caf::intrusive_ptr<behavior_base>;
 
 
 //An abstract behavior that implements some of commonly used features by 
