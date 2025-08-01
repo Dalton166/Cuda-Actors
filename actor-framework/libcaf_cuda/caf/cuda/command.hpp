c961d8a9e98d5cc72c240cf2655aa7f00a85b0cb
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-08-01 12:27:36 -0600

    updated command to inherit from a new class called base_command which always returns mem refs. the new current command will always return an output buffer.

--- actor-framework/libcaf_cuda/caf/cuda/command.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/command.hpp
@@ -22,75 +22,96 @@
 #include <caf/send.hpp>
 #include <caf/message.hpp>
 
+
 namespace caf::cuda {
 
+//this class will always launch and schedule a kernel 
+//execution and return a tuple of mem ref's
 template <class Actor, class... Ts>
-class command : public ref_counted {
+class base_command : public ref_counted {
 public:
   template <typename... Us>
-  command(caf::message msg,
-        caf::actor self,
-        program_ptr program,
-        nd_range dims,
-        int id, 
-        Us&&... xs)
-  : msg_(msg),
-    program_(program),
-    dims_(dims),
-    actor_id(id)
-    { 
-  dev_ = platform::create()->schedule(id);
-  static_assert(sizeof...(Us) == sizeof...(Ts), "Argument count mismatch");
-}
+  base_command(caf::message msg,
+               caf::actor self,
+               program_ptr program,
+               nd_range dims,
+               int id,
+               Us&&... xs)
+    : msg_(std::move(msg)),
+      program_(std::move(program)),
+      dims_(std::move(dims)),
+      actor_id(id) {
+    dev_ = platform::create()->schedule(id);
+    static_assert(sizeof...(Us) == sizeof...(Ts), "Argument count mismatch");
+  }
 
+  virtual ~base_command() = default;
 
- ~command() = default;
+  // Unpacks a caf message and calls launch_kernel_mem_ref and returns its result  
+  virtual std::tuple<mem_ref<raw_t<Ts>>...> enqueue() {
+    // Step 1: Unpack message
+    auto unpacked = msg_.template get_as<std::tuple<Ts...>>(0);
 
+    // Step 2: Launch kernel via centralized utility
+    CUfunction kernel = program_->get_kernel(dev_->getId());
+    return dev_->launch_kernel_mem_ref(kernel, dims_, unpacked, actor_id);
+  }
 
-  //unpacks a caf message and calls launch_kernel_mem_ref and returns its result  
- std::tuple<mem_ref<raw_t<Ts>>...> enqueue() {
-  // Step 1: Unpack message
-  auto unpacked = msg_.template get_as<std::tuple<Ts...>>(0);
-
-  // Step 2: Launch kernel via centralized utility
-  CUfunction kernel = program_->get_kernel(dev_->getId());
-  return dev_->launch_kernel_mem_ref(kernel, dims_, unpacked, actor_id);
-}
-
-
+  // intrusive_ptr ref counting friends
+  template <class A, class... S>
+  friend void intrusive_ptr_add_ref(base_command<A, S...>* ptr);
 
   template <class A, class... S>
-  friend void intrusive_ptr_add_ref(command<A, S...>* ptr);
+  friend void intrusive_ptr_release(base_command<A, S...>* ptr);
 
-  template <class A, class... S>
-  friend void intrusive_ptr_release(command<A, S...>* ptr);
-
-private:
+protected:
   program_ptr program_;
   nd_range dims_;
   int actor_id;
   device_ptr dev_;
   caf::message msg_;
-
-   auto launch_kernel(program_ptr program,
-                     const nd_range& range,
-                     std::tuple<mem_ptr<raw_t<Ts>>...> args,
-                     int actor_id) -> std::vector<output_buffer> {
-    CUfunction kernel = program->get_kernel(dev_->getId());
-    return dev_->launch_kernel(kernel, range, args, actor_id);
-  }
 };
 
 // intrusive_ptr reference counting
 template <class Actor, class... Ts>
-inline void intrusive_ptr_add_ref(command<Actor, Ts...>* ptr) {
+inline void intrusive_ptr_add_ref(base_command<Actor, Ts...>* ptr) {
   ++(ptr->ref_count);
 }
 
 template <class Actor, class... Ts>
-inline void intrusive_ptr_release(command<Actor, Ts...>* ptr) {
+inline void intrusive_ptr_release(base_command<Actor, Ts...>* ptr) {
   if (--(ptr->ref_count) == 0)
     delete ptr;
 }
+
+// This class really just returns an output buffer instead of a tuple of mem refs
+template <class Actor, class... Ts>
+class command : public base_command<Actor, Ts...> {
+public:
+  using base = base_command<Actor, Ts...>;
+
+  template <typename... Us>
+  command(caf::message msg,
+          caf::actor self,
+          program_ptr program,
+          nd_range dims,
+          int id,
+          Us&&... xs)
+    : base(std::move(msg),
+           std::move(self),
+           std::move(program),
+           std::move(dims),
+           id,
+           std::forward<Us>(xs)...) {}
+
+  // Override enqueue to return collected output_buffers instead of mem_refs tuple
+  std::vector<output_buffer> enqueue() {
+    // Call base enqueue to get tuple of mem_refs
+    auto mem_refs_tuple = base::enqueue();
+
+    // Convert mem_refs to output_buffers via helper
+    return collect_output_buffers_helper(mem_refs_tuple);
+  }
+};
 
 } // namespace caf::cuda
