e3e7cfefb78fe1f4bfb232171b5b12815346f32b
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-08-11 09:44:53 -0600

    Added a extract vector helper function.

--- actor-framework/libcaf_cuda/caf/cuda/helpers.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/helpers.hpp
@@ -14,4 +14,17 @@ namespace caf::cuda {
 	CAF_CUDA_EXPORT std::vector<char> load_cubin(const std::string&);
 	CAF_CUDA_EXPORT bool compile_nvrtc_program(const char* source, CUdevice device, std::vector<char>& ptx_out);
 
+
+	// Extract first matching vector<T> from outputs
+template <typename T>
+std::optional<std::vector<T>> extract_vector(const std::vector<output_buffer>& outputs) {
+    for (const auto& out : outputs) {
+        if (auto ptr = std::get_if<std::vector<T>>(&out.data)) {
+            return *ptr; // copy found vector<T>
+        }
+    }
+    return std::nullopt; // none found
+}
+
+
 }
