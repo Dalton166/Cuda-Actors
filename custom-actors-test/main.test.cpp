b4fd0a99cbdd8084dcb794235200ee41fd850d57
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-08-11 09:27:56 -0600

    Initial commit .

---
+++ custom-actors-test/main.test.cpp
@@ -0,0 +1,75 @@
+
+#include "main.test.hpp"
+
+#include <caf/all.hpp>
+#include <caf/cuda/all.hpp>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include <algorithm>
+#include <numeric>
+#include "caf/actor_registry.hpp"
+
+
+
+using namespace caf;
+using namespace std::chrono_literals;
+
+const char* kernel_code = R"(
+extern "C" __global__
+void compare_strings(const char* a, const char* b, int* result, int * length) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < * length) {
+        result[idx] = (a[idx] == b[idx]) ? 1 : 0;
+    }
+}
+)";
+
+const char* matrixMulKernel2 = R"(
+extern "C" __global__
+void matrixMul(const int* a, const int* b, int* c, int *N_val) {
+    int N = *N_val;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    if (row < N && col < N) {
+        int temp = 0;
+        for (int k = 0; k < N; ++k) {
+            temp += a[row * N + k] * b[k * N + col];
+        }
+        c[row * N + col] = temp;
+    }
+}
+)";
+
+
+const char* matrixMulKernel = R"(
+extern "C" __global__
+void matrixMul(const int* a, const int* b, int* c, int N) {
+    //printf("%d\n",N);
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    if (row < N && col < N) {
+        int temp = 0;
+        for (int k = 0; k < N; ++k) {
+            temp += a[row * N + k] * b[k * N + col];
+        }
+        c[row * N + col] = temp;
+    }
+}
+)";
+
+
+
+
+
+void caf_main(caf::actor_system& sys) {
+  caf::cuda::manager::init(sys);
+
+}
+
+
+
+
+CAF_MAIN()
