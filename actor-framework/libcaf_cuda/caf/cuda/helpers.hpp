7aa9474a00ba2c0ada17856cdd41c8bcefeb94fd
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-08-11 10:43:13 -0600

    Updated extract vector to not have an optional since return is always guarenteed.

--- actor-framework/libcaf_cuda/caf/cuda/helpers.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/helpers.hpp
@@ -14,10 +14,9 @@ namespace caf::cuda {
 	CAF_CUDA_EXPORT std::vector<char> load_cubin(const std::string&);
 	CAF_CUDA_EXPORT bool compile_nvrtc_program(const char* source, CUdevice device, std::vector<char>& ptx_out);
 
-
-	// Extract first matching vector<T> from outputs
+// Extract first matching vector<T> from outputs
 template <typename T>
-std::optional<std::vector<T>> extract_vector(const std::vector<output_buffer>& outputs) {
+std::optional<std::vector<T>> extract_vector_or_empty(const std::vector<output_buffer>& outputs) {
     for (const auto& out : outputs) {
         if (auto ptr = std::get_if<std::vector<T>>(&out.data)) {
             return *ptr; // copy found vector<T>
@@ -25,6 +24,19 @@ std::optional<std::vector<T>> extract_vector(const std::vector<output_buffer>& o
     }
     return std::nullopt; // none found
 }
+
+// Extract first matching vector<T> from outputs
+template <typename T>
+std::vector<T> extract_vector(const std::vector<output_buffer>& outputs) {
+    for (const auto& out : outputs) {
+        if (auto ptr = std::get_if<std::vector<T>>(&out.data)) {
+            return *ptr; // copy and return matching vector<T>
+        }
+    }
+    return {}; // no matching vector found, return empty vector
+}
+
+
 
 
 }
