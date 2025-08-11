954e94f65b419a0d4e4ce33386c6341259b11542
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-08-11 11:05:21 -0600

    Moved generate matrix kernel to its own file.

--- custom-actors-test/mmul.cu
+++ custom-actors-test/mmul.cu
@@ -1,4 +1,3 @@
-#include <curand_kernel.h>
 // mmul.cu
 extern "C" __global__
 void matrixMul(const int* a, const int* b, int* c, int N) {
@@ -12,20 +11,6 @@ void matrixMul(const int* a, const int* b, int* c, int N) {
         }
         c[row * N + col] = temp;
     }
-}
-
-
-//generate_random_matrix
-extern "C" __global__
-void generate_random_matrix(int* matrix, int total_elements, int seed, int max_val) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx >= total_elements) return;
-
-    curandState state;
-    curand_init((unsigned long long)seed, idx, 0, &state);
-
-    unsigned int r = curand(&state);
-    matrix[idx] = r % max_val;
 }
 
 
