79339943765c4cd4d544ebc03b425b02f7cdbf7f
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-08-11 14:11:22 -0600

    Implemented a new run async method.

--- actor-framework/libcaf_cuda/caf/cuda/command_runner.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/command_runner.hpp
@@ -8,8 +8,11 @@ template <class... Ts>
 class command_runner {
 public:
   using command_t = command<caf::actor, Ts...>;
-
+  using base_command_t = base_command<caf::actor,Ts ...>;
   // Run a command with provided args, returning enqueue()'s result.
+  // this is a synchronous operation that this will techincally block an actor 
+  // but it does ensure that the data in the output buffers is actually in
+  // the output buffers 
   template <class... Us>
   auto run(program_ptr program, nd_range dims, int actor_id, Us&&... xs) {
     static_assert(sizeof...(Us) == sizeof...(Ts),
@@ -21,6 +24,39 @@ public:
         std::forward<Us>(xs)...);
     return cmd->enqueue();
   }
+
+
+  //this is the asynchronous version of command, it returns a tuple of mem_ptr's
+  //the data can then be copied back to host or sent to other actors
+ template <class... Us>
+  auto run_async(program_ptr program, nd_range dims, int actor_id, Us&&... xs) {
+    static_assert(sizeof...(Us) == sizeof...(Ts),
+                  "Number of arguments must match Ts...");
+    auto cmd = caf::make_counted<base_command_t>(
+        std::move(program),
+        std::move(dims),
+        actor_id,
+        std::forward<Us>(xs)...);
+    return cmd->enqueue();
+  }
+
+  //this is the asynchronous version of command, it returns a tuple of mem_ptr's
+  //the data can then be copied back to host or sent to other actors
+ template <class... Us>
+  auto run_async(program_ptr program, nd_range dims, int actor_id,int device_number, Us&&... xs) {
+    static_assert(sizeof...(Us) == sizeof...(Ts),
+                  "Number of arguments must match Ts...");
+    auto cmd = caf::make_counted<base_command_t>(
+        std::move(program),
+        std::move(dims),
+        actor_id,
+	device_number,
+        std::forward<Us>(xs)...);
+    return cmd->enqueue();
+  }
+
+
+  
 };
 
 } //namespace caf::cuda
