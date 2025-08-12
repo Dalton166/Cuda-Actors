4aea4e7866a625adb23a7a5930bfa13aead32afe
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-08-12 14:12:08 -0600

    implemented a async mmul test.

--- custom-actors-test/main.test.cpp
+++ custom-actors-test/main.test.cpp
@@ -63,9 +63,11 @@ void matrixMul(const int* a, const int* b, int* c, int N) {
 using mmulCommand = caf::cuda::command_runner<in<int>,in<int>,out<int>,in<int>>;
 using matrixGenCommand = caf::cuda::command_runner<out<int>,in<int>,in<int>,in<int>>;
 
+using mmulAsyncCommand = caf::cuda::command_runner<caf::cuda::mem_ptr<int>,caf::cuda::mem_ptr<int>,out<int>,in<int>>;
+
 mmulCommand mmul;
 matrixGenCommand randomMatrix;
-
+mmulAsyncCommand mmulAsync;
 
 
 void serial_matrix_multiply(const std::vector<int>& a,
@@ -221,13 +223,148 @@ void run_mmul_test(caf::actor_system& sys, int matrix_size, int num_actors) {
 }
 
 
+// Stateful actor behavior
+caf::behavior mmul_async_actor_fun(caf::stateful_actor<mmul_actor_state>* self) {
+  return {
+    // 1st handler: Just int N, and who to send the matrices to
+    [=](int N, std::vector<caf::actor> receivers) {
+
+	/*
+	 * Unfortuanley libraries such as curand cannot be linked with cubins 
+	 * making it incompatable with this software for right now
+	 * its not really random, just a matrix filled with 5's
+	 */
+        caf::cuda::manager& mgr = caf::cuda::manager::get();
+        //create the program and configure the dimesnions of the kernel
+        auto program = mgr.create_program_from_cubin("../mmul.cubin","generate_random_matrix");
+	int THREADS = 256;
+	int BLOCKS = (N*N + THREADS - 1) / THREADS;
+  	caf::cuda::nd_range dim(BLOCKS,1, 1, THREADS,1, 1);
+
+	//tag the arguments so that caf::cuda knows what to do with them	
+         auto arg1 = caf::cuda::create_out_arg(N*N); //output buffer indicate its size, caf::cuda will handle the rest
+          auto arg2 = caf::cuda::create_in_arg(N*N); //matrix size
+          auto arg3 = caf::cuda::create_in_arg(1234); //seed
+	  auto arg4 = caf::cuda::create_in_arg(9999); //max valux
+	  
+	  int device_number= 0;
+
+
+	  //launch kernels and collect their outputs
+	  auto tempA = randomMatrix.run_async(program,dim, self -> state().id,device_number,arg1,arg2,arg3,arg4);
+	  auto tempB = randomMatrix.run_async(program,dim, self -> state().id,device_number,arg1,arg2,arg3,arg4);
+	  caf::cuda::mem_ptr<int> matrixA =  std::get<0>(tempA);
+	  caf::cuda::mem_ptr<int> matrixB = std::get<0>(tempB);
+
+	  //ensure the data is actually done being worked on
+	  matrixA -> synchronize();
+	  matrixB -> synchronize();
+
+
+
+
+	  //cpu code
+	  //std::vector<int> matrixA(N*N);
+	  //std::vector<int> matrixB(N*N);
+
+	  // std::generate(matrixA.begin(), matrixA.end(), []() { return rand() % 10; });
+	   //std::generate(matrixB.begin(), matrixB.end(), []() { return rand() % 10; });
+
+
+	  //broadcast the result out to receviers.
+	  for (auto actor: receivers) {
+	  
+		  self->mail(matrixA,matrixB,N,device_number).send(actor);
+	  }
+
+    },
+
+    // 2nd handler: GPU atom + matrices + N, launches a kenrel and sends its result to itself for verification
+    [=](const caf::cuda::mem_ptr<int> matrixA,
+        const caf::cuda::mem_ptr<int> matrixB, int N,int device_number) {
+ 
+
+  caf::cuda::manager& mgr = caf::cuda::manager::get();
+
+  //create program and dims   
+  auto program = mgr.create_program_from_cubin("../mmul.cubin","matrixMul");
+  const int THREADS = 32;
+  const int BLOCKS = (N + THREADS - 1) / THREADS;
+  caf::cuda::nd_range dims(BLOCKS, BLOCKS, 1, THREADS, THREADS, 1);
+
+    //create args
+    auto arg1 = matrixA;
+    auto arg2 = matrixB;
+    auto arg3 = caf::cuda::create_out_arg(N*N);
+    auto arg4 = caf::cuda::create_in_arg(N);
+
+
+    auto tempC = mmulAsync.run(program,dims,self -> state().id,device_number,arg1,arg2,arg3,arg4);
+    
+    std::vector<int> matrixC = caf::cuda::extract_vector<int>(tempC);
+
+    //verify its own result 
+    self -> mail(matrixA,matrixB,matrixC,N).send(self);
+
+    },
+
+    // 3rd handler: CPU atom + matrices + N
+    [=](const std::vector<int>& matrixA,
+        const std::vector<int>& matrixB, const std::vector<int> matrixC, int N) {
+       
+	 std::vector<int> result(N*N);
+
+	 serial_matrix_multiply(matrixA,matrixB,result,N);
+
+	 if (result == matrixC) {
+	 
+		 std::cout << "actor with id " <<  self->state().id << " references match\n";
+	 
+	 }
+
+	 else {
+	    std::cout << "actor with id " <<  self->state().id << " references did not match\n";
+	 }
+
+	 self-> quit();
+
+    }
+  };
+}
+
+
+
+
+
+
+void run_async_mmul_test(caf::actor_system& sys, int matrix_size, int num_actors) {
+  if (num_actors < 1) {
+    std::cerr << "[ERROR] Number of actors must be >= 1\n";
+    return;
+  }
+
+  // Spawn num_actors actors running the mmul behavior
+  std::vector<caf::actor> actors;
+  actors.reserve(num_actors);
+  for (int i = 0; i < num_actors; ++i) {
+    actors.push_back(sys.spawn(mmul_async_actor_fun));
+  }
+
+  // Actor 0 generates matrices and broadcasts to others 
+  caf::anon_mail(matrix_size, actors).send(actors[0]);
+
+   sys.await_all_actors_done();
+}
+
+
 
 
 
 void caf_main(caf::actor_system& sys) {
   caf::cuda::manager::init(sys);
 
-  run_mmul_test(sys,100,2);
+  run_mmul_test(sys,100,200);
+  run_async_mmul_test(sys,100,2);
 
 }
 
