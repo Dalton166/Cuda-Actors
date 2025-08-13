fa89094763e5c2458de7cf72e43ac749b8936cd6
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-08-13 12:24:02 -0600

    Added overloads for shared memory support.

--- actor-framework/libcaf_cuda/caf/cuda/command_runner.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/command_runner.hpp
@@ -4,14 +4,21 @@
 #include "caf/cuda/nd_range.hpp"
 #include "caf/cuda/platform.hpp"
 
+
 namespace caf::cuda {
+
 template <class... Ts>
 class command_runner {
 public:
   using command_t = command<caf::actor, Ts...>;
-  using base_command_t = base_command<caf::actor,Ts ...>;
+  using base_command_t = base_command<caf::actor, Ts...>;
+
+  // ----------------------------------------
+  // Synchronous versions
+  // ----------------------------------------
+
   // Run a command with provided args, returning enqueue()'s result.
-  // this is a synchronous operation that this will techincally block an actor 
+  // this is a synchronous operation that this will technically block an actor 
   // but it does ensure that the data in the output buffers is actually in
   // the output buffers 
   template <class... Us>
@@ -26,30 +33,42 @@ public:
     return cmd->enqueue();
   }
 
-
-
-  //same thing as above just with device number, enabling a user to select
-  //what device they want, without actually having to worry about having it
-  //on the same device
+  // Same as above but allows the user to specify shared memory
   template <class... Us>
-  auto run(program_ptr program, nd_range dims, int actor_id,int device_number, Us&&... xs) {
+  auto run(program_ptr program, nd_range dims, int actor_id, int shared_mem, Us&&... xs) {
     static_assert(sizeof...(Us) == sizeof...(Ts),
                   "Number of arguments must match Ts...");
     auto cmd = caf::make_counted<command_t>(
         std::move(program),
         std::move(dims),
         actor_id,
-	device_number,
+        shared_mem,
         std::forward<Us>(xs)...);
     return cmd->enqueue();
   }
 
+  // Same as above but allows the user to select shared memory and device number
+  template <class... Us>
+  auto run(program_ptr program, nd_range dims, int actor_id, int shared_mem, int device_number, Us&&... xs) {
+    static_assert(sizeof...(Us) == sizeof...(Ts),
+                  "Number of arguments must match Ts...");
+    auto cmd = caf::make_counted<command_t>(
+        std::move(program),
+        std::move(dims),
+        actor_id,
+        shared_mem,
+        device_number,
+        std::forward<Us>(xs)...);
+    return cmd->enqueue();
+  }
 
+  // ----------------------------------------
+  // Asynchronous versions
+  // ----------------------------------------
 
-
-  //this is the asynchronous version of command, it returns a tuple of mem_ptr's
-  //the data can then be copied back to host or sent to other actors
- template <class... Us>
+  // this is the asynchronous version of command, it returns a tuple of mem_ptr's
+  // the data can then be copied back to host or sent to other actors
+  template <class... Us>
   auto run_async(program_ptr program, nd_range dims, int actor_id, Us&&... xs) {
     static_assert(sizeof...(Us) == sizeof...(Ts),
                   "Number of arguments must match Ts...");
@@ -61,29 +80,45 @@ public:
     return cmd->base_enqueue();
   }
 
-  //this is the asynchronous version of command, it returns a tuple of mem_ptr's
-  //the data can then be copied back to host or sent to other actors
- template <class... Us>
-  std::tuple<mem_ptr<raw_t<Ts>>...> run_async(program_ptr program, nd_range dims, int actor_id,int device_number, Us&&... xs) {
+  // Same as above but allows the user to specify shared memory
+  template <class... Us>
+  auto run_async(program_ptr program, nd_range dims, int actor_id, int shared_mem, Us&&... xs) {
     static_assert(sizeof...(Us) == sizeof...(Ts),
                   "Number of arguments must match Ts...");
     auto cmd = caf::make_counted<base_command_t>(
         std::move(program),
         std::move(dims),
         actor_id,
-	device_number,
+        shared_mem,
         std::forward<Us>(xs)...);
     return cmd->base_enqueue();
   }
 
+  // Same as above but allows the user to select shared memory and device number
+  template <class... Us>
+  auto run_async(program_ptr program, nd_range dims, int actor_id, int shared_mem, int device_number, Us&&... xs) {
+    static_assert(sizeof...(Us) == sizeof...(Ts),
+                  "Number of arguments must match Ts...");
+    auto cmd = caf::make_counted<base_command_t>(
+        std::move(program),
+        std::move(dims),
+        actor_id,
+        shared_mem,
+        device_number,
+        std::forward<Us>(xs)...);
+    return cmd->base_enqueue();
+  }
 
- //destroys a stream for an actor given its id
- void release_stream_for_actor(int actor_id) {
- 
-	 auto plat = platform::create();
-	 plat -> release_streams_for_actor(actor_id);
- }
-  
+  // ----------------------------------------
+  // Stream release
+  // ----------------------------------------
+
+  // destroys a stream for an actor given its id
+  void release_stream_for_actor(int actor_id) {
+    auto plat = platform::create();
+    plat->release_streams_for_actor(actor_id);
+  }
 };
 
-} //namespace caf::cuda
+} // namespace caf::cuda
+
