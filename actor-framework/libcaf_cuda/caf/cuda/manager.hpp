a8b7847f661b2754223b85965db0c2cde0aa0183
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-08-13 10:18:56 -0600

    Added a create program from fatbin stub.

--- actor-framework/libcaf_cuda/caf/cuda/manager.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/manager.hpp
@@ -108,6 +108,12 @@ public:
   program_ptr create_program_from_cubin(const std::string& filename,
                                                const char* kernel_name);
 
+  program_ptr create_program_from_fatbin(const std::string& filename,
+                                               const char* kernel_name);
+
+
+
+
   template <bool PassConfig, class Result, class... Ts>
   caf::actor spawn(const char*,
                    program_ptr,
