eb925d17e490598e480fedb9859774dc818bc59e
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-08-13 12:49:24 -0600

    Updated commands constructor to have optional arguments.

--- actor-framework/libcaf_cuda/caf/cuda/command.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/command.hpp
@@ -2,235 +2,139 @@
 
 #include <tuple>
 #include <vector>
-#include <functional>
-#include <atomic>
 #include <iostream>
 
 #include <caf/abstract_actor.hpp>
-#include <caf/actor_cast.hpp>
-#include <caf/logger.hpp>
-#include <caf/raise_error.hpp>
-#include <caf/response_promise.hpp>
 #include <caf/intrusive_ptr.hpp>
-#include <caf/detail/type_list.hpp>
+#include <caf/message.hpp>
 
 #include "caf/cuda/global.hpp"
 #include "caf/cuda/nd_range.hpp"
 #include "caf/cuda/platform.hpp"
-#include "caf/cuda/arguments.hpp"
-#include "caf/cuda/device.hpp"
-#include <caf/send.hpp>
-#include <caf/message.hpp>
 #include "caf/cuda/mem_ref.hpp"
+#include "caf/cuda/device.hpp"
 
 namespace caf::cuda {
 
-//this class will always launch and schedule a kernel 
-//execution and return a tuple of mem ref's
+// -----------------------------------------------------------------------------
+// Base command: schedules kernel, returns tuple of mem_ptrs
+// -----------------------------------------------------------------------------
 template <class Actor, class... Ts>
 class base_command : public ref_counted {
 public:
+  // ---------------------------------------------------------------------------
+  // Constructor: variadic arguments first, then optional shared_memory/device_number
+  // ---------------------------------------------------------------------------
   template <typename... Us>
-  base_command(caf::message msg,
-               program_ptr program,
+  base_command(program_ptr program,
                nd_range dims,
-               int id,
-               Us&&... xs)
-    : msg_(std::move(msg)),
-      program_(std::move(program)),
-      dims_(std::move(dims)),
-      actor_id(id) {
-    dev_ = platform::create()->schedule(id);
-    using_message = true; 
-    static_assert(sizeof...(Us) == sizeof...(Ts), "Argument count mismatch");
-  }
-  template <typename... Us>
-  base_command(
-               program_ptr program,
-               nd_range dims,
-               int id,
-               Us&&... xs)
+               int actor_id,
+               Us&&... xs,
+               int shared_memory_ = 0,
+               int device_number = -1)
     : program_(std::move(program)),
       dims_(std::move(dims)),
-      actor_id(id),
-      kernel_args(std::make_tuple(std::forward<Us>(xs)...))
-       	{
-    
-	dev_ = platform::create()->schedule(id);
-    static_assert(sizeof...(Us) == sizeof...(Ts), "Argument count mismatch");
+      actor_id(actor_id),
+      kernel_args(std::make_tuple(std::forward<Us>(xs)...)),
+      shared_memory(shared_memory_)
+  {
+      if (device_number == -1)
+          dev_ = platform::create()->schedule(actor_id);
+      else
+          dev_ = platform::create()->schedule(actor_id, device_number);
   }
-
-
-
-  template <typename... Us>
-  base_command(
-               program_ptr program,
-               nd_range dims,
-               int id,
-	       int device_number,
-               Us&&... xs)
-    : program_(std::move(program)),
-      dims_(std::move(dims)),
-      actor_id(id),
-      kernel_args(std::make_tuple(std::forward<Us>(xs)...))
-       	{
-    
-	dev_ = platform::create()->schedule(id,device_number);
-    static_assert(sizeof...(Us) == sizeof...(Ts), "Argument count mismatch");
-  }
-
-
-
-
 
   virtual ~base_command() = default;
 
-  // Unpacks a caf message and calls launch_kernel_mem_ref and returns its result  
+  // ---------------------------------------------------------------------------
+  // Launch kernel and return tuple of mem_ptrs
+  // ---------------------------------------------------------------------------
   virtual std::tuple<mem_ptr<raw_t<Ts>>...> base_enqueue() {
-    
-    //std::cout << "Unpacking message\n";
-    // Step 1: Unpack message
-     if (using_message) {
-     auto unpacked = unpack_args(std::index_sequence_for<Ts...>{});
-    
-    //std::cout << "Launching kernel\n";
-    // Step 2: Launch kernel via centralized utility
-    CUfunction kernel = program_->get_kernel(dev_->getId());
-    return dev_->launch_kernel_mem_ref(kernel, dims_, unpacked, actor_id);
-     }
-     else {
-     //Launch kernel via centralized utility
-    CUfunction kernel = program_->get_kernel(dev_->getId());
-    return dev_->launch_kernel_mem_ref(kernel, dims_, kernel_args, actor_id);
-     
-     
-     }
+      CUfunction kernel = program_->get_kernel(dev_->getId());
 
-
-     
+      if (using_message) {
+          auto unpacked = unpack_args(std::index_sequence_for<Ts...>{});
+          return dev_->launch_kernel_mem_ref(kernel, dims_, unpacked, actor_id, shared_memory);
+      } else {
+          return dev_->launch_kernel_mem_ref(kernel, dims_, kernel_args, actor_id, shared_memory);
+      }
   }
 
-  // intrusive_ptr ref counting friends
+  // ---------------------------------------------------------------------------
+  // CAF intrusive_ptr helpers
+  // ---------------------------------------------------------------------------
   template <class A, class... S>
   friend void intrusive_ptr_add_ref(base_command<A, S...>* ptr);
 
   template <class A, class... S>
   friend void intrusive_ptr_release(base_command<A, S...>* ptr);
 
-
   template <size_t... Is>
-auto unpack_args(std::index_sequence<Is...>) {
-  return std::make_tuple(msg_.get_as<Ts>(Is)...);
-}
-
-
+  auto unpack_args(std::index_sequence<Is...>) {
+      return std::make_tuple(msg_.get_as<Ts>(Is)...);
+  }
 
 protected:
   program_ptr program_;
   nd_range dims_;
   int actor_id;
   device_ptr dev_;
+  int shared_memory;
   caf::message msg_;
-  std::tuple<Ts ...> kernel_args;
+  std::tuple<Ts...> kernel_args;
   bool using_message = false;
-
 };
 
-// intrusive_ptr reference counting
+// intrusive_ptr helpers
 template <class Actor, class... Ts>
-inline void intrusive_ptr_add_ref(base_command<Actor, Ts...>* ptr) {
-  ptr->ref();
-}
+inline void intrusive_ptr_add_ref(base_command<Actor, Ts...>* ptr) { ptr->ref(); }
 
 template <class Actor, class... Ts>
-inline void intrusive_ptr_release(base_command<Actor, Ts...>* ptr) {
-  ptr -> deref();
-}
+inline void intrusive_ptr_release(base_command<Actor, Ts...>* ptr) { ptr->deref(); }
 
-// This class really just returns an output buffer instead of a tuple of mem refs
-// also handles mem ref cleanup for you 
+// -----------------------------------------------------------------------------
+// Command: returns vector of output_buffer
+// -----------------------------------------------------------------------------
 template <class Actor, class... Ts>
 class command : public base_command<Actor, Ts...> {
 public:
   using base = base_command<Actor, Ts...>;
 
   template <typename... Us>
-  command(caf::message msg,
-          program_ptr program,
+  command(program_ptr program,
           nd_range dims,
-          int id,
-          Us&&... xs)
-    : base(std::move(msg),
-           std::move(program),
+          int actor_id,
+          Us&&... xs,
+          int shared_memory_ = 0,
+          int device_number = -1)
+    : base(std::move(program),
            std::move(dims),
-           id,
-           std::forward<Us>(xs)...) {}
- 
-  template <typename... Us>
-  command(
-          program_ptr program,
-          nd_range dims,
-          int id,
-          Us&&... xs)
-    : base(
-           std::move(program),
-           std::move(dims),
-           id,
-           std::forward<Us>(xs)...) {}
+           actor_id,
+           std::forward<Us>(xs)...,
+           shared_memory_,
+           device_number)
+  {}
 
-
- template <typename... Us>
-  command(
-          program_ptr program,
-          nd_range dims,
-          int id,
-	  int device_number,
-          Us&&... xs)
-    : base(
-           std::move(program),
-           std::move(dims),
-           id,
-	   device_number,
-           std::forward<Us>(xs)...) {}
-
-
-
-
-  // Override enqueue to return collected output_buffers instead of mem_refs tuple
+  // ---------------------------------------------------------------------------
+  // Enqueue returns vector of output_buffer
+  // ---------------------------------------------------------------------------
   std::vector<output_buffer> enqueue() {
-    // Call base enqueue to get tuple of mem_refs
-    auto mem_refs = base::base_enqueue();
-
-    // Convert mem_refs to output_buffers via helper
-    auto result = base::dev_ -> collect_output_buffers(mem_refs);
-    /*
-     * We shouldnt need to manually free the mem refs
-     * since they are intrusive pointers caf can handle this automatically
-     * 
-    for_each_tuple(mem_refs, [](auto& mem) {
-      if (mem)
-        mem->reset();
-    });
-
-    */
-
-     return result;
+      auto mem_refs = base::base_enqueue();
+      return base::dev_->collect_output_buffers(mem_refs);
   }
 
-
-
+  // ---------------------------------------------------------------------------
+  // Tuple iteration helpers
+  // ---------------------------------------------------------------------------
   template <typename Tuple, typename Func, size_t... Is>
   void for_each_tuple_impl(Tuple& t, Func&& f, std::index_sequence<Is...>) {
-    (f(std::get<Is>(t)), ...);
+      (f(std::get<Is>(t)), ...);
   }
 
   template <typename... Is, typename Func>
   void for_each_tuple(std::tuple<Is...>& t, Func&& f) {
-    for_each_tuple_impl(t, std::forward<Func>(f), std::index_sequence_for<Is...>{});
+      for_each_tuple_impl(t, std::forward<Func>(f), std::index_sequence_for<Is...>{});
   }
-
-
-
 };
 
 } // namespace caf::cuda
