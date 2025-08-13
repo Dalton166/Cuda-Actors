a632c54dc4a3efdd1f3383a28098d74731d1da70
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-08-13 13:49:34 -0600

    Got a shared memory handler working ... somewhat.

--- custom-actors-test/main.test.cpp
+++ custom-actors-test/main.test.cpp
@@ -9,11 +9,17 @@
 #include <numeric>
 #include <random>
 #include "caf/actor_registry.hpp"
+//#include <caf/atoms.hpp>
 
 
 
 using namespace caf;
 using namespace std::chrono_literals;
+
+// Define a custom type ID block for custom actors
+CAF_ADD_ATOM(cuda,shared_mem)
+
+
 
 const char* kernel_code = R"(
 extern "C" __global__
@@ -243,7 +249,9 @@ caf::behavior mmul_async_actor_fun(caf::stateful_actor<mmul_actor_state>* self) 
 	  auto arg4 = caf::cuda::create_in_arg(9999); //max valux
 	  
           auto arg3B = caf::cuda::create_in_arg(rand()); //seed
-	  int device_number= 74;
+	  int device_number= 74; //arbitary number to show that 
+				 //can give illusion of selecting gpus that are
+				 //not there
 
 
 	  //launch kernels and collect their outputs
@@ -271,7 +279,7 @@ caf::behavior mmul_async_actor_fun(caf::stateful_actor<mmul_actor_state>* self) 
 	  //broadcast the result out to receviers.
 	  for (auto actor: receivers) {
 	  
-		  self->mail(matrixA,matrixB,N,device_number).send(actor);
+		  self->mail(3,matrixA,matrixB,N,device_number).send(actor);
 	  }
 
     },
@@ -307,6 +315,40 @@ caf::behavior mmul_async_actor_fun(caf::stateful_actor<mmul_actor_state>* self) 
 
     },
 
+ // 3nd handler: GPU atom + matrices + N, launches a kenrel using shared memory and sends its result to itself for verification
+    [=](int x,const caf::cuda::mem_ptr<int> matrixA,
+        const caf::cuda::mem_ptr<int> matrixB, int N,int device_number) {
+ 
+
+  caf::cuda::manager& mgr = caf::cuda::manager::get();
+
+  //create program and dims   
+  auto program = mgr.create_program_from_cubin("../shared_mmul.cubin","matrixMul");
+  const int THREADS = 32;
+  const int BLOCKS = (N + THREADS - 1) / THREADS;
+  caf::cuda::nd_range dims(BLOCKS, BLOCKS, 1, THREADS, THREADS, 1);
+
+  int shared_mem = 8192; //we need 8KB of shared memory here
+    //create args
+    auto arg1 = matrixA;
+    auto arg2 = matrixB;
+    auto arg3 = caf::cuda::create_out_arg(N*N);
+    auto arg4 = caf::cuda::create_in_arg(N);
+
+
+    auto tempC = mmulAsync.run(program,dims,self -> state().id,shared_mem,device_number,arg1,arg2,arg3,arg4);
+
+    std::vector<int> matrix1 = matrixA -> copy_to_host();
+    std::vector<int> matrix2 = matrixB -> copy_to_host();    
+    std::vector<int> matrixC = caf::cuda::extract_vector<int>(tempC,2);
+
+    //verify its own result 
+    self -> mail(matrix1,matrix2,matrixC,N).send(self);
+
+    },
+
+
+
     // 3rd handler: CPU atom + matrices + N
     [=](const std::vector<int>& matrixA,
     const std::vector<int> &matrixB,
@@ -323,7 +365,9 @@ caf::behavior mmul_async_actor_fun(caf::stateful_actor<mmul_actor_state>* self) 
         std::cout << "actor with id " << self->state().id << " references did not match\n";
 
     }
-        auto print_matrix = [N](const std::vector<int>& mat, const std::string& name) {
+
+    /*
+    auto print_matrix = [N](const std::vector<int>& mat, const std::string& name) {
             std::cout << name << ":\n";
             for (int i = 0; i < N; ++i) {
                 for (int j = 0; j < N; ++j) {
@@ -338,7 +382,7 @@ caf::behavior mmul_async_actor_fun(caf::stateful_actor<mmul_actor_state>* self) 
         print_matrix(matrixB, "Matrix B");
         print_matrix(result, "Result Matrix");
         print_matrix(matrixC, "GPU Result Matrix");
-    
+    */
 
     self->quit();
     }
@@ -370,14 +414,11 @@ void run_async_mmul_test(caf::actor_system& sys, int matrix_size, int num_actors
 }
 
 
-
-
-
 void caf_main(caf::actor_system& sys) {
   caf::cuda::manager::init(sys);
 
   //run_mmul_test(sys,100,200);
-  run_async_mmul_test(sys,1,2);
+  run_async_mmul_test(sys,100,200);
 
 }
 
