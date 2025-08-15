efd8cd6b68f731e230324d4837f21a0a99d4d298
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-08-15 12:58:17 -0600

    Added double and float variants of matrixMul.

--- float-double-test/mmul.cu
+++ float-double-test/mmul.cu
@@ -14,3 +14,33 @@ void matrixMul(const int* a, const int* b, int* c, int N) {
 }
 
 
+extern "C" __global__
+void matrixMulFloat(const float* a, const float* b, float* c, int N) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < N && col < N) {
+        float temp = 0.0f;
+        for (int k = 0; k < N; ++k) {
+            temp += a[row * N + k] * b[k * N + col];
+        }
+        c[row * N + col] = temp;
+    }
+}
+
+extern "C" __global__
+void matrixMulDouble(const double* a, const double* b, double* c, int N) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < N && col < N) {
+        double temp = 0.0;
+        for (int k = 0; k < N; ++k) {
+            temp += a[row * N + k] * b[k * N + col];
+        }
+        c[row * N + col] = temp;
+    }
+}
+
+
+
