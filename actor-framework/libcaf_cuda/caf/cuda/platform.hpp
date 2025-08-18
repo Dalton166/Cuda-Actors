a0f51932e16bb62d522a5fbb4b1aacb5568ecf5d
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-08-18 10:11:49 -0600

    Updated inline documentation.

--- actor-framework/libcaf_cuda/caf/cuda/platform.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/platform.hpp
@@ -15,23 +15,35 @@
 
 namespace caf::cuda {
 
+//A container that has access to all the devices and a scheduler 
+//to select which device an actor should go onto
+//is An actors first point of contact when they want to access the device
+//Actors are not allowed to have any access to the device class 
 class CAF_CUDA_EXPORT platform : public ref_counted {
 public:
   friend class program;
   template <class T, class... Ts>
   friend intrusive_ptr<T> caf::make_counted(Ts&&...);
 
-  static platform_ptr create();
+  static platform_ptr create(); //singleton method that creates or gets platform
 
-  const std::string& name() const;
+
+  const std::string& name() const; 
   const std::string& vendor() const;
   const std::string& version() const;
+  //returns a list of devices 
   const std::vector<device_ptr>& devices() const;
 
+  //returns a single device given its id
   device_ptr getDevice(int id);
+  //returns the scheduler being used 
   scheduler* get_scheduler();
+
+  //returns the device that a command should use
   device_ptr schedule(int actor_id);
+  //returns the device that a command should used 
   device_ptr schedule(int actor_id,int device_number);
+  //releases a stream for an actor
   void release_streams_for_actor(int actor_id);
 
 private:
