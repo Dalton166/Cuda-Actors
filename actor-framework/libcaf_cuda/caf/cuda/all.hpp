41598398a1df6f9bedd1b6fcddfefe99bea576f1
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-08-18 10:39:43 -0600

    Added more includes to all.hpp.

--- actor-framework/libcaf_cuda/caf/cuda/all.hpp
+++ actor-framework/libcaf_cuda/caf/cuda/all.hpp
@@ -1,22 +1,3 @@
-/******************************************************************************
- *                       ____    _    _____                                   *
- *                      / ___|  / \  |  ___|    C++                           *
- *                     | |     / _ \ | |_       Actor                         *
- *                     | |___ / ___ \|  _|      Framework                     *
- *                      \____/_/   \_|_|                                      *
- *                                                                            *
- * Copyright (C) 2011 - 2016                                                  *
- * Raphael Hiesgen <raphael.hiesgen (at) haw-hamburg.de>                      *
- *                                                                            *
- * Distributed under the terms and conditions of the BSD 3-Clause License or  *
- * (at your option) under the terms and conditions of the Boost Software      *
- * License 1.0. See accompanying files LICENSE and LICENSE_ALTERNATIVE.       *
- *                                                                            *
- * If you did not receive a copy of the license files, see                    *
- * http://opensource.org/licenses/BSD-3-Clause and                            *
- * http://www.boost.org/LICENSE_1_0.txt.                                      *
- ******************************************************************************/
-
 #pragma once
 
 // CUDA manager include
@@ -24,4 +5,5 @@
 #include "caf/cuda/nd_range.hpp"
 #include "caf/cuda/program.hpp"
 #include "caf/cuda/command_runner.hpp"
-
+#include "caf/cuda/helpers.hpp"
+#include "caf/cuda/actor_facade.hpp"
