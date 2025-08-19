06a6f44a75a94820abf7600ccc937b04acb447e2
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-08-19 10:49:57 -0600

    Removed reudnant doc-strings.

--- actor-framework/libcaf_cuda/src/platform.cpp
+++ actor-framework/libcaf_cuda/src/platform.cpp
@@ -6,19 +6,18 @@ namespace caf::cuda {
 platform_ptr platform::create() {
   static platform_ptr instance;
   if (!instance) {
-    //std::cout << "[platform] constructor called\n";
     instance = make_counted<platform>();
   }
   return instance;
 }
 
+//constructor
 platform::platform() {
   int device_count = 0;
   check(cuDeviceGetCount(&device_count), "cuDeviceGetCount");
   devices_.resize(device_count);
   contexts_.resize(device_count);
 
-  //std::cout << "Device count is " << device_count << "\n";
 
   std::vector<std::string> device_names(device_count);
 
@@ -45,10 +44,8 @@ platform::platform() {
   //we turn off multi gpu support
   if (device_count > 1 && all_same) {
     scheduler_ = std::make_unique<multi_device_scheduler>();
-    //std::cout << "Using multi-device scheduler (heterogeneous GPUs)\n";
   } else {
     scheduler_ = std::make_unique<single_device_scheduler>();
-    //std::cout << "Using single-device scheduler (homogeneous or single GPU)\n";
   }
 
   scheduler_->set_devices(devices_);
@@ -60,8 +57,7 @@ platform::platform() {
 
 
 platform::~platform() {
-  std::cout << "Destroying platform\n";
-  // Context cleanup is handled in device destructors
+	//no-op
 }
 
 const std::string& platform::name() const {
@@ -93,13 +89,11 @@ scheduler* platform::get_scheduler() {
 
 device_ptr platform::schedule(int actor_id) {
   
-	//std::cout <<  "schedule called\n";
 	return scheduler_->schedule(actor_id);
 }
 
 device_ptr platform::schedule(int actor_id,int device_number) {
   
-	//std::cout <<  "schedule called\n";
 	return scheduler_->schedule(actor_id,device_number);
 }
 
